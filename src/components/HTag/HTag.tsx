942da2d8a1d8d7e6802442871d2159bcd022e0c0
andrew28092002 <andreiastanaev2002@gmail.com>
2023-04-17 11:17:10 +0500

    update htag

--- src/components/HTag/HTag.tsx
+++ src/components/HTag/HTag.tsx
@@ -1,19 +1,35 @@
-import React, { ReactNode } from "react";
-import styles from './HTag.module.css'
+import React, { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
+import styles from "./HTag.module.css";
 
-interface HTagProps {
+interface HTagProps
+  extends DetailedHTMLProps<
+    HTMLAttributes<HTMLHeadingElement>,
+    HTMLHeadingElement
+  > {
   tag: "h1" | "h2" | "h3";
   children: ReactNode;
 }
 
-const HTag = ({ tag, children }: HTagProps) => {
+const HTag = ({ tag, children, className, ...props }: HTagProps) => {
   switch (tag) {
     case "h1":
-      return <h1 className={styles.h1}>{children}</h1>;
+      return (
+        <h1 className={styles.h1} {...props}>
+          {children}
+        </h1>
+      );
     case "h2":
-      return <h2 className={styles.h2}>{children}</h2>;
+      return (
+        <h2 className={styles.h2} {...props}>
+          {children}
+        </h2>
+      );
     case "h3":
-      return <h3 className={styles.h3}>{children}</h3>;
+      return (
+        <h3 className={styles.h3} {...props}>
+          {children}
+        </h3>
+      );
     default:
       return <></>;
   }
