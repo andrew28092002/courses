5b53c1c34a350cabc1c448b8a6a96ae52d0d9133
andrew28092002 <andreiastanaev2002@gmail.com>
2023-04-28 14:09:22 +0500

    header

---
+++ src/layout/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React, { DetailedHTMLProps, FC, HTMLAttributes, ReactNode } from "react";
+import styles from "./Header.module.css";
+
+interface HeaderProps
+  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
+
+const Header: FC<HeaderProps> = ({ ...props }) => {
+  return <div {...props}>Sidebar</div>;
+};
+
+export default Header;
--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import React, { DetailedHTMLProps, FC, HTMLAttributes, ReactNode } from "react";
 import styles from "./MainLayout.module.css";
 import Sidebar from "../Sidebar/Sidebar";
+import Header from "../Header/Header";
 
 interface MainLayoutProps {
   children: ReactNode;
