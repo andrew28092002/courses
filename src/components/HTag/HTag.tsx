cd405f60095cd44811e53a2a61303f1d3f75aa06
andrew28092002 <andreiastanaev2002@gmail.com>
2023-05-04 21:27:14 +0500

    change color

--- src/components/HTag/HTag.tsx
+++ src/components/HTag/HTag.tsx
@@ -14,19 +14,28 @@ const HTag: FC<HTagProps> = ({ tag, children, className, ...props }) => {
   switch (tag) {
     case "h1":
       return (
-        <h1 className={`${className || ""} ${styles.h1}`} {...props}>
+        <h1
+          className={`${className || ""} ${styles.h} ${styles.h1}`}
+          {...props}
+        >
           {children}
         </h1>
       );
     case "h2":
       return (
-        <h2 className={`${className || ""} ${styles.h2}`} {...props}>
+        <h2
+          className={`${className || ""} ${styles.h} ${styles.h2}`}
+          {...props}
+        >
           {children}
         </h2>
       );
     case "h3":
       return (
-        <h3 className={`${className || ""} ${styles.h3}`} {...props}>
+        <h3
+          className={`${className || ""} ${styles.h} ${styles.h3}`}
+          {...props}
+        >
           {children}
         </h3>
       );
--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -4,8 +4,14 @@ import styles from "./Footer.module.css";
 interface FooterProps
   extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
 
-const Footer: FC<FooterProps> = ({ ...props }) => {
-  return <div {...props}>Footer</div>;
+const Footer: FC<FooterProps> = ({ className, ...props }) => {
+  return (
+    <footer className={`${className || ''} ${styles.footer}`} {...props}>
+      <div>OwlTop © 2020 - X Все права защищены</div>
+      <a href="#" target="_blank">Пользовательское соглашение</a>
+      <a href="#" target="_blank">Политика конфиденциальности</a>
+    </footer>
+  );
 };
 
 export default Footer;
