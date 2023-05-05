b5e9832ba67dc1d0422c67d91601795d08ccc85d
andrew28092002 <andreiastanaev2002@gmail.com>
2023-05-05 12:31:57 +0500

    wrap layout in provider

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -3,13 +3,13 @@ import styles from "./MainLayout.module.css";
 import Sidebar from "../Sidebar/Sidebar";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import { AppContextProvider, IAppContext } from "@/context/app.context";
 
 interface MainLayoutProps {
   children: ReactNode;
 }
 
 const MainLayout: FC<MainLayoutProps> = ({ children }) => {
-  
   return (
     <div className={styles.wrapper}>
       <Header className={styles.header} />
@@ -23,10 +23,12 @@ const MainLayout: FC<MainLayoutProps> = ({ children }) => {
 };
 
 export const withLayout =
-  <T extends Record<string, unknown>>(Component: FC<T>) =>
+  <T extends Record<string, unknown> & IAppContext>(Component: FC<T>) =>
   (props: T) =>
     (
-      <MainLayout>
-        <Component {...props} />
-      </MainLayout>
+      <AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
+        <MainLayout>
+          <Component {...props} />
+        </MainLayout>
+      </AppContextProvider>
     );
