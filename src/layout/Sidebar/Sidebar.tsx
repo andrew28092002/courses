bc981553102027abf1c4ce29526de936f82bb024
andrew28092002 <andreiastanaev2002@gmail.com>
2023-05-05 19:14:11 +0500

    sidebar

--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -1,12 +1,15 @@
 import React, { DetailedHTMLProps, FC, HTMLAttributes, ReactNode } from "react";
 import styles from "./Sidebar.module.css";
 import Menu from "../Menu/Menu";
+import Logo from './../logo.svg'
 
 interface SidebarProps
   extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
 
-const Sidebar: FC<SidebarProps> = ({ ...props }) => {
-  return <div {...props}>
+const Sidebar: FC<SidebarProps> = ({className, ...props }) => {
+  return <div className={`${className || ''} ${styles.sidebar}`} {...props}>
+    <Logo className={styles.logo}/>
+    <div><input type="text" placeholder="Поиск..." /></div>
     <Menu />
   </div>;
 };
