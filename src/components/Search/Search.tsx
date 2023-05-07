41ad0383aafd38dc688011e58f5dc8fedd4f6dba
andrew28092002 <andreiastanaev2002@gmail.com>
2023-05-07 13:41:43 +0500

    search functions

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -3,20 +3,40 @@ import styles from "./Search.module.css";
 import Input from "../Input/Input";
 import Button from "../Button/Button";
 import SearchIcon from "./search.svg";
+import { useRouter } from "next/router";
 
 interface SearchProps
   extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
 
 const Search: FC<SearchProps> = ({ className, ...props }) => {
   const [search, setSearch] = useState<string>("");
+  const router = useRouter()
+
+  const goToSearch = () => {
+    router.push({
+        pathname: '/search',
+        query: {
+            q: search
+        }
+    })
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter'){
+        goToSearch()
+    }
+  }
+
   return (
-    <div className={`${className || ""} ${styles.search}`}>
+    <div className={`${className || ""} ${styles.search}`} {...props}>
       <Input
         placeholder="Поиск..."
+        className={styles.input}
         value={search}
         onChange={(e) => setSearch(e.target.value)}
+        onKeyDown={handleKeyDown}
       />
-      <Button appearance="primary" className={styles.button} onClick={() => {}}>
+      <Button appearance="primary" className={styles.button} onClick={goToSearch}>
         <SearchIcon />
       </Button>
     </div>
